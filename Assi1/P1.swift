ed53c1dc19090dadcf8d72d63ef17fba897736f3
HinalLukhi <89583620+HinalLukhi@users.noreply.github.com>
2021-10-04 16:20:55 +0000

    Initial commit

---
+++ Assi1/P1.swift
@@ -0,0 +1,39 @@
+enum NumError: Error {
+
+    case negative
+
+    var message: String {
+        switch self {
+        case .negative: return "ERROR: Marks cannot be negative"
+        }
+    }
+}
+
+func checkEvenOdd(n: Int) throws {
+   if(n<0)
+        throw NumError.negative
+    }
+    else
+    {
+      if(n%2==0)
+      {
+        print(n+" is Even Number..")
+      }
+      else
+      {
+        print(n+" is Odd Number..")
+      }
+    }
+}
+
+do 
+{
+    try checkEvenOdd(n: -10)
+} 
+catch 
+{
+    if let error = error as? NumError 
+    {
+        print(error.message)
+    }
+} 
