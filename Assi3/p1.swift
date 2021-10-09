8bda20d347772352fa7a7c6c4e7c5b45b3f669d6
Hinal Lukhi <89583620+HinalLukhi@users.noreply.github.com>
2021-10-09 15:14:45 +0000

    Assi3

--- Assi3/p1.swift
+++ Assi3/p1.swift
@@ -1,8 +1,66 @@
-for i in 1...5
+var c:String?
+repeat
 {
-    for j in 1...i
+    print("----Pattern List----")
+    print("1. Number right triangle")
+    print("2. Sequential number right triangle")
+    print("3. Pascal triangle")
+    print("Enter your Choice : ")
+    if let ch=Int(readLine()!)
     {
-        print(j,terminator : " ")
+        switch(ch)
+        {
+          case 1:
+                print("Enter number of rows: ")
+                if let n=Int(readLine()!)
+                {
+                  for i in 1...n
+                  {
+                      for j in 1...i
+                      {
+                          print(j,terminator : " ")
+                      }
+                      print(" ")
+                  }
+                }
+
+            case 2: 
+                print("Enter number of rows: ")
+                if let n=Int(readLine()!)
+                {
+                  var j=1
+                  for i in 1...n
+                  {
+                      for _ in 1...i
+                      {
+                          print(j,terminator : " ")
+                          j=j+1
+                      }
+                      print(" ")
+                  }
+                  
+                }
+
+            case 3:
+                print("Enter number of rows: ")
+                if let n=Int(readLine()!)
+                {
+                  for i in 1...n
+                  {
+                      var n=1
+                      for j in 1...i
+                      {
+                          print(n,terminator : "")
+                          n=n*(i-j)/j
+                      }
+                      print(" ")
+                  }
+                }
+            default:
+                print("Wrong Choice")
+        }
+        print("Do You Want To Continue (y/n) ? ")
+        c=readLine()!
     }
-    print(" ")
-}
+}while(c=="y")
+
